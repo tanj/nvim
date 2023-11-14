29e257f26c6a110e3cafb26887d4c7972dddc5a4
John te Bokkel <john@powercoreeng.com>
2023-11-13 20:31:51 -0800

    add comments and buffer listing maps

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -84,6 +84,11 @@ return require('packer').startup(function(use)
   use("github/copilot.vim")
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
-
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
 end)
 
